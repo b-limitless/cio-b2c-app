26a8c6060f46106d7281967bc598eb3d6e44e5af
Bharat Shah <akashche72@gmail.com>
2023-08-22 18:24:38 +0530

    worked on few part of navigation

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from './header.module.scss';
+import styles from  './header.module.scss';
 import Image from 'next/image';
 
 
 export default function Header() {
   return (
-    <header className="styles header">
-        <div className="styles col">
+    <header className={styles.header}>
+        <div className={styles.col}>
         <Image
         src={'/icon/logo.svg'}
         width={206}
@@ -14,9 +14,11 @@ export default function Header() {
         alt='logo'
         />
         </div>
-        <div className="styles col">
-            <div className="styles nav">
+        <div className={styles.col}>
+            <div className={styles.nav}>
                 <ul>
+                    <input type="radio" className={styles.navigation__radio} name="navigation" id="febric" />
+                    <label htmlFor="febric">
                     <li>
                         <span>
                             Febric
@@ -31,8 +33,47 @@ export default function Header() {
                             </Image>
                         </span>
                     </li>
+                    </label>
 
+                    <input type="radio" className={styles.navigation__radio} name="navigation" id="style" />
+                    <label htmlFor="style">
                     <li>
+                        <span>
+                            Style
+                        </span>
+                        <span>
+                            <Image
+                            src={'/icon/arrow-right.svg'}
+                            height={11}
+                            width={6.29}
+                            alt=''
+                            >
+                            </Image>
+                        </span>
+                    </li>
+                    </label>
+                    
+                    <input type="radio" className={styles.navigation__radio} name="navigation" id="accent" />
+                    <label htmlFor="accent">
+                    <li>
+                        <span>
+                            accent
+                        </span>
+                        <span>
+                            <Image
+                            src={'/icon/arrow-right.svg'}
+                            height={11}
+                            width={6.29}
+                            alt=''
+                            >
+                            </Image>
+                        </span>
+                    </li>
+                    </label>
+                   
+                   
+
+                    {/* <li>
                         <span>
                             style
                         </span>
@@ -59,16 +100,16 @@ export default function Header() {
                             >
                             </Image>
                         </span>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
         </div>
 
-        <div className="styles side__menu">
-            <div className="styles menu">
+        <div className={styles.col}>
+            <div className={styles.menu}>
                 clothing
             </div>
-            <div className="styles icon">
+            <div className={styles.icon}>
                 <Image
                 src={'/icon/cart.svg'}
                 width={32}
--- pages/home/page.tsx
+++ pages/home/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'; 
+import React from 'react';
 import styles from './home.module.scss';
 import Header from 'components/Header/Header';
 
 export default function Home() {
-  return (
-    <div className="styles page__container">
-        <Header/>
-    </div>
-  )
+    return (
+        <div className="styles page__container">
+            <Header />
+            <div className="styles mid__content">
+                <div className="styles col">
+                    <div className="styles contents">
+                        <div className="styles normal">
+                            Made to Measure
+                        </div>
+                        <div className="styles bold">
+                            that fit you perfectly
+                        </div>
+                        <div className="styles description">
+                            Shirts that fit you perfectly. Choose a custom dress shirt designed by you. Make a statement with a made to measure shirt perfect for any occasions, whether it {'\\'} s business or casual we will tailor the perfect men s dress shirt for you. Buy the best custom dress shirt online.
+                        </div>
+                        <button>
+                            <span>Design shirt</span>
+                            <span>
+                                <img src="/icon/arrow-right-bold.svg" alt="" />
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div className="styles col">
+                    <img src="/img/man.png" alt="" />
+                </div>
+            </div>
+
+        </div>
+    )
 }
