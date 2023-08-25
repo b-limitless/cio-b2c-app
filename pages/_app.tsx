78ab98854456a4d86a0dae536bdc1d068f4c4898
Bharat Shah <akashche72@gmail.com>
2023-08-25 20:32:05 +0530

    worked on rresponsive design for the customize template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { Poppins } from 'next/font/google';
 import '/styles/index.scss';
+import Head from 'next/head';
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800'],
@@ -11,9 +12,15 @@ const poppins = Poppins({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
     <main className={poppins.className}>
       <Component {...pageProps} />
     </main>
+    </>
+    
   )
 }
 
