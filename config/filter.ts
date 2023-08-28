acef5be06f58ca7c6834346b5f599162ead1462a
Bharat Shah <akashche72@gmail.com>
2023-08-28 20:16:12 +0530

    worked on filter module to controll filter with the config file

---
+++ config/filter.ts
@@ -0,0 +1,60 @@
+export const filterData = [
+  {
+    label: "CATEGORY",
+    value: "CATEGORY",
+    type: "text",
+    childrens: [
+      {
+        label: "NEW",
+        value: "New",
+      },
+      {
+        label: "NEW",
+        value: "New",
+      },
+      {
+        label: "NEW",
+        value: "New",
+      },
+    ],
+  },
+  {
+    label: "material",
+    value: "material",
+    type: "text",
+    childrens: [
+      {
+        label: "NEW",
+        value: "New",
+      },
+      {
+        label: "NEW",
+        value: "New",
+      },
+      {
+        label: "NEW",
+        value: "New",
+      },
+    ],
+  },
+  {
+    label: "something",
+    value: "something",
+    type: "color",
+
+    childrens: [
+      {
+        label: "NEW",
+        value: "New",
+      },
+      {
+        label: "NEW",
+        value: "New",
+      },
+      {
+        label: "NEW",
+        value: "New",
+      },
+    ],
+  },
+];
--- pages/customize/Febric/Filter.tsx
+++ pages/customize/Febric/Filter.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './filter.module.scss';
 import Image from 'next/image';
 import { CheckboxWithLabel } from 'components/Checkbox/Checkbox';
+import { filterData } from 'config/filter';
 
 const colors = [{
     title: "Red",
@@ -28,6 +29,60 @@ const colors = [{
     hex: "blue"
 }
 ]
+
+interface ColorInterface {
+    title: string;
+    hex: string;
+    i: number;
+}
+
+type filtertype = 'color' | 'text';
+
+interface FilterItemInterface {
+    label: string;
+    value: string;
+    childrens: any[];
+    type: string; 
+}
+
+
+
+const Color = ({ title, hex, i }: ColorInterface) => {
+    return <>
+        <input type="checkbox" hidden name="color" id={title + hex + i} className={styles.color__checkbox} />
+        <label htmlFor={title + hex + i} className={styles.label}>
+            <span className={styles.color} style={{ backgroundColor: `${hex}` }}></span>
+        </label>
+    </>
+}
+
+
+const FilterItem = ({label, value, childrens, type}: FilterItemInterface) => {
+    return <div className={styles.item}>
+        <input hidden type='checkbox' name='' id={value} className={styles.options__checkbox} />
+
+        <label htmlFor={value} className={styles.label}>
+            <span className={styles.title}>
+               {label}
+            </span>
+            <span className={styles.indicator}>
+                <Image width={14} height={8} src={'/icon/arrow-up.svg'} alt=''></Image>
+            </span>
+        </label>
+
+        <div className={styles.form__element}>
+            <div className={styles.wrapper}>
+                {childrens.map((children, i) => <CheckboxWithLabel key={`${i}-filter-checkbox`}label={children.label} />)}
+                {/* <CheckboxWithLabel label='New' />
+                <CheckboxWithLabel label='Eco/Organic' />
+                <CheckboxWithLabel label='New' />
+                <CheckboxWithLabel label='Eco/Organic' />
+                <CheckboxWithLabel label='New' />
+                <CheckboxWithLabel label='Eco/Organic' /> */}
+            </div>
+        </div>
+    </div>
+}
 export default function Filter() {
     return (
         <div className={styles.filter}>
@@ -48,7 +103,14 @@ export default function Filter() {
                 </span>
             </div>
             <div className={styles.toggle__container}>
-                <div className={styles.item}>
+                {filterData.map((filter, i) => <FilterItem 
+                         key = {`filter-item-${i}`}
+                         label={filter.label} value={filter.value}
+                         childrens={filter.childrens}
+                         type={filter.type}
+                         />
+                         )}
+                {/* <div className={styles.item}>
                     <input hidden type='checkbox' name='' id='category' className={styles.options__checkbox} />
 
                     <label htmlFor='category' className={styles.label}>
@@ -109,26 +171,14 @@ export default function Filter() {
                     </label>
 
                     <div className={styles.form__element}>
-                        <div className={styles.wrapper + ' ' + styles.colors}>
-                            <>
-                            <input  type="checkbox" hidden name="color" id="red" className={styles.color__checkbox}/>
-                            <label htmlFor="red" className={styles.label}>
-                                <span className={styles.color}></span>
-                            </label>
-                            </>
-                            
-                            <>
-                                <input hidden type="checkbox" name="color" id="green" className={styles.color__checkbox}/>
-                                <label htmlFor="green" className={styles.label}>
-                                    <span className={styles.color}></span>
-                                </label>
-                            </>
-                            
+                        <div className={styles.colors}>
+
+                            {colors.map((color, i) => <Color i={i} key={`${color}-${i}`} title={color.title} hex={color.hex} />)}
                         </div>
                     </div>
                 </div>
 
-                {/* <div className={styles.item}>
+                <div className={styles.item}>
                     <input hidden type='checkbox' name='' id='material' className={styles.options__checkbox} />
 
                     <label htmlFor='material' className={styles.label}>
@@ -151,6 +201,8 @@ export default function Filter() {
                         </div>
                     </div>
                 </div> */}
+
+
             </div>
         </div>
     )
