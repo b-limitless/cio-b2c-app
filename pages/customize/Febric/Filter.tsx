aa111da4925b39b4122ea78cb1bb509a94c86c26
Bharat Shah <akashche72@gmail.com>
2023-08-28 17:14:39 +0530

    designing ui to display the color

--- pages/customize/Febric/Filter.tsx
+++ pages/customize/Febric/Filter.tsx
@@ -2,10 +2,36 @@ import React from 'react'
 import styles from './filter.module.scss';
 import Image from 'next/image';
 import { CheckboxWithLabel } from 'components/Checkbox/Checkbox';
+
+const colors = [{
+    title: "Red",
+    hex: "#FF0000"
+},
+{
+    title: "green",
+    hex: "green"
+},
+{
+    title: "blue",
+    hex: "blue"
+},
+{
+    title: "Red",
+    hex: "#FF0000"
+},
+{
+    title: "green",
+    hex: "green"
+},
+{
+    title: "blue",
+    hex: "blue"
+}
+]
 export default function Filter() {
     return (
         <div className={styles.filter}>
-            <div className={styles.header }>
+            <div className={styles.header}>
                 <span className={styles.title}>
                     FILTER
                 </span>
@@ -23,52 +49,108 @@ export default function Filter() {
             </div>
             <div className={styles.toggle__container}>
                 <div className={styles.item}>
-                        <input hidden type='checkbox' name='' id='category' className={styles.options__checkbox}/>
-                        
-                        <label htmlFor='category' className={styles.label}>
-                            <span className={styles.title}>
-                                CATEGORY
-                            </span>
-                            <span className={styles.indicator}>
-                                <Image width={14} height={8} src={'/icon/arrow-up.svg'} alt=''></Image>
-                            </span>
-                        </label>
-                        
-                        <div className={styles.form__element}>
-                            <div className={styles.wrapper}>
-                                <CheckboxWithLabel label='New'/> 
-                                <CheckboxWithLabel label='Eco/Organic'/>
-                                <CheckboxWithLabel label='New'/>
-                                <CheckboxWithLabel label='Eco/Organic'/>
-                                <CheckboxWithLabel label='New'/>
-                                <CheckboxWithLabel label='Eco/Organic'/>
-                            </div>
+                    <input hidden type='checkbox' name='' id='category' className={styles.options__checkbox} />
+
+                    <label htmlFor='category' className={styles.label}>
+                        <span className={styles.title}>
+                            CATEGORY
+                        </span>
+                        <span className={styles.indicator}>
+                            <Image width={14} height={8} src={'/icon/arrow-up.svg'} alt=''></Image>
+                        </span>
+                    </label>
+
+                    <div className={styles.form__element}>
+                        <div className={styles.wrapper}>
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
                         </div>
+                    </div>
                 </div>
 
                 <div className={styles.item}>
-                        <input hidden type='checkbox' name='' id='material' className={styles.options__checkbox}/>
-                        
-                        <label htmlFor='material' className={styles.label}>
-                            <span className={styles.title}>
-                                MATERIAL
-                            </span>
-                            <span className={styles.indicator}>
-                                <Image width={14} height={8} src={'/icon/arrow-up.svg'} alt=''></Image>
-                            </span>
-                        </label>
-                        
-                        <div className={styles.form__element}>
-                            <div className={styles.wrapper}>
-                                <CheckboxWithLabel label='New'/> 
-                                <CheckboxWithLabel label='Eco/Organic'/>
-                                <CheckboxWithLabel label='New'/>
-                                <CheckboxWithLabel label='Eco/Organic'/>
-                                <CheckboxWithLabel label='New'/>
-                                <CheckboxWithLabel label='Eco/Organic'/>
-                            </div>
+                    <input hidden type='checkbox' name='' id='material' className={styles.options__checkbox} />
+
+                    <label htmlFor='material' className={styles.label}>
+                        <span className={styles.title}>
+                            MATERIAL
+                        </span>
+                        <span className={styles.indicator}>
+                            <Image width={14} height={8} src={'/icon/arrow-up.svg'} alt=''></Image>
+                        </span>
+                    </label>
+
+                    <div className={styles.form__element}>
+                        <div className={styles.wrapper}>
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
                         </div>
+                    </div>
                 </div>
+
+                <div className={styles.item}>
+                    <input hidden type='checkbox' name='' id='colors' className={styles.options__checkbox} />
+
+                    <label htmlFor='colors' className={styles.label}>
+                        <span className={styles.title}>
+                            COLORS
+                        </span>
+                        <span className={styles.indicator}>
+                            <Image width={14} height={8} src={'/icon/arrow-up.svg'} alt=''></Image>
+                        </span>
+                    </label>
+
+                    <div className={styles.form__element}>
+                        <div className={styles.wrapper + ' ' + styles.colors}>
+                            <>
+                            <input  type="checkbox" hidden name="color" id="red" className={styles.color__checkbox}/>
+                            <label htmlFor="red" className={styles.label}>
+                                <span className={styles.color}></span>
+                            </label>
+                            </>
+                            
+                            <>
+                                <input hidden type="checkbox" name="color" id="green" className={styles.color__checkbox}/>
+                                <label htmlFor="green" className={styles.label}>
+                                    <span className={styles.color}></span>
+                                </label>
+                            </>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                {/* <div className={styles.item}>
+                    <input hidden type='checkbox' name='' id='material' className={styles.options__checkbox} />
+
+                    <label htmlFor='material' className={styles.label}>
+                        <span className={styles.title}>
+                            MATERIAL
+                        </span>
+                        <span className={styles.indicator}>
+                            <Image width={14} height={8} src={'/icon/arrow-up.svg'} alt=''></Image>
+                        </span>
+                    </label>
+
+                    <div className={styles.form__element}>
+                        <div className={styles.wrapper}>
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
+                            <CheckboxWithLabel label='New' />
+                            <CheckboxWithLabel label='Eco/Organic' />
+                        </div>
+                    </div>
+                </div> */}
             </div>
         </div>
     )
