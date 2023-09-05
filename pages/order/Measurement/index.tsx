475074d6b886845bc5ee3739717fc06c4a7ce554
Bharat Shah <akashche72@gmail.com>
2023-09-05 18:55:48 +0530

    worked on response part for the measurement

--- pages/order/Measurement/index.tsx
+++ pages/order/Measurement/index.tsx
@@ -6,6 +6,10 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './measurement.module.scss';
 
+const ages = [{name: "0-18", value: "0-18"}];
+const height = [{name: "5", value: "5"}];
+const inches = [{name: "1", value: "1"}];
+
 export default function Measurment() {
     return (
         <div className={styles.measurement__container}>
@@ -16,7 +20,6 @@ export default function Measurment() {
                 <p className={styles.description}>
                     We are going to create your body measurements profile. All we need is some basic information.
                 </p>
-
                 <div className={styles.form__group}>
                 <div className={styles.form__row}>
                     <Input label='Full Name' />
@@ -25,50 +28,41 @@ export default function Measurment() {
                     Change Unite feet/lb or cm/kg
                 </p>
                 <div className={styles.form__row}>
-                    
                         <Select
-                        options={[]}
+                        options={height}
                         value=''
-                        label=''
+                        label='Height/Feet'
                         onChange={() => { }}
                     />
-
                     <Select
-                        options={[]}
+                        options={inches}
                         value=''
-                        label=''
-                        onChange={() => { }}
-                    />
-
-                    <Select
-                        options={[]}
-                        value=''
-                        label=''
+                        label='inch'
                         onChange={() => { }}
                     />
                     <Select
                         options={[]}
                         value=''
+                        label='weight/lb'
+                        onChange={() => { }}
+                    />
+                    <Input
+                        options={ages}
+                       
                         label='Age'
+                        type='number'
                         onChange={() => { }}
                     />
-                    
-                    
                 </div>
                 <p className={styles.accurate}>
                     The more accurate you give your height and weight, the better the system will help you take your measurements.
                 </p>
-
                 </div>
-                
-
                 <div className={styles.actions}>
                     <Button variant='primary' type='square'>
                         Next
                     </Button>
                 </div>
-
-
             </div>
             <div className={styles.video__guide}>
                 <Image src='/img/video.png' width={1190} height={670} alt='' />
@@ -84,7 +78,6 @@ export default function Measurment() {
                         and take your measurements later
                     </span>
                 </div>
-
                 <div className={styles.col}>
                     <Image src='/icon/time.svg' width={60} height={60} alt='time' />
                     <span className={styles.sub}>Are you in a hurry?</span>
