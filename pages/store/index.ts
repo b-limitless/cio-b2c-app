6347ea59d64330ac56357eceb8254001f8703f9a
Bharat Shah <akashche72@gmail.com>
2023-11-21 18:53:34 +0545

    adding model slicer and its functions reducer

--- pages/store/index.ts
+++ pages/store/index.ts
@@ -1,59 +1,68 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction } from '@reduxjs/toolkit';
 
-const model = {
-    collar: {
-        id: 12,
-        model: 'URL for the model to load from CDN'
-    }, 
-    scuff: {
-        id: 13,
-        model: 'URL for the model to load'
-    }, 
-    sleeves: {
-        id: 13,
-        model: 'URL for the model to load'
-    },
-    checkpocket: {
-        id: 13,
-        model: 'URL for the model to load'
-    }
+const modelProperties = {
+  collar: {
+    id: 12,
+    model: 'URL for the model to load from CDN',
+  },
+  scuff: {
+    id: 13,
+    model: 'URL for the model to load',
+  },
+  sleeves: {
+    id: 13,
+    model: 'URL for the model to load',
+  },
+  checkpocket: {
+    id: 13,
+    model: 'URL for the model to load',
+  },
+};
+
+type ModelType = typeof modelProperties;
+type modelKeys = keyof ModelType;
+
+interface UpdateModelAction {
+  key: modelKeys;
+  payload: ModelType[modelKeys];
 }
-
-const accent = {
-    collar: {
-        selected: 'By Default', 
-        febric: {
-            id: null,
-            url: 'http://'
-        }
-    }, 
-    cuff: {
-        selected: 'By Default',
-        febric: {
-            id: null, 
-            url: 'http://'
-        }
-    }, 
-}
-
-type modeType = typeof model;
-type accentType = typeof accent;
 
 interface initialState {
-    model: modeType | null;
-    accent: accentType | null;
+  model: ModelType | null;
 }
 
-const initialState:initialState = {
-    model:null,
-    accent: null
-}
+const initialState: initialState = {
+  model: null,
+  //   accent: null,
+};
 
 const modelSlice = createSlice({
-    name: 'model', 
-    initialState,
-    reducers:{
-
-    }
+  name: 'model',
+  initialState,
+  reducers: {
+    updateModel: (state: initialState, action: PayloadAction<UpdateModelAction>) => {
+      return {
+        ...state,
+        [action.payload.key]: action.payload.payload,
+      };
+    },
+  },
 });
 
+// const accent = {
+//   collar: {
+//     selected: 'By Default',
+//     febric: {
+//       id: null,
+//       url: 'http://',
+//     },
+//   },
+//   cuff: {
+//     selected: 'By Default',
+//     febric: {
+//       id: null,
+//       url: 'http://',
+//     },
+//   },
+// };
