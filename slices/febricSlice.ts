45a80c69250de5388017ef80d61dfbcf50b5d7bf
Bharat Shah <akashche72@gmail.com>
2024-01-03 18:34:32 +0400

    need to manage febric state which is applied to whole model from different state and actionc

---
+++ slices/febricSlice.ts
@@ -0,0 +1,43 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { defaultFebric } from 'config/default';
+
+export type TFebric = {
+  title: string;
+  price: number;
+  material?: string;
+  tone?: string;
+  febricTypes?: string;
+  id: number;
+  model: string;
+  originalImageUrl?: string;
+  code?: string;
+  label?: string;
+  season?: string;
+};
+
+const initialState: TFebric = {
+  id: 12,
+  model: defaultFebric,
+  price: 10,
+  title: '',
+  material: 'Cotton',
+  tone: 'light',
+  febricTypes: 'Cotton',
+  season: 'summer',
+  label: 'default',
+  code: 'default',
+  originalImageUrl: defaultFebric,
+};
+
+const febricSlice = createSlice({
+  name: 'updateCartIndex',
+  initialState,
+  reducers: {
+    updateFebric: (state: TFebric, action: PayloadAction<TFebric>) => {
+      return { ...state, ...action.payload };
+    },
+  },
+});
+
+export const { updateFebric } = febricSlice.actions;
+export default febricSlice.reducer;
