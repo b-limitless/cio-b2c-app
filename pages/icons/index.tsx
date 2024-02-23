245f932472ea589d08116e18478ff4c693c023fd
Bharat Shah <akashche72@gmail.com>
2024-02-23 19:13:19 +0400

    adding required icons

---
+++ pages/icons/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const count = new Array(40).fill(0);
+
+export default function Index() {
+
+  const PrintIcons = () => {
+    let result = [];
+    
+    result = count.map((_, i) => <><div className='icon-col'><span key={i} className={`shirt-icon icon-${39 + i}`}></span><span>{39 + i}</span></div></>);
+
+    return result;
+
+  }
+  
+  return (
+    <div className="icons-container">
+        <PrintIcons/>
+    </div>
+   
+  )
+}
